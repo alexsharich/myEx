6f4e5095296adc1525fafe3e4c96e7f8d37f42b1
alexsharich <alexsharich@gmail.com>
2024-09-22 00:14:08 +0300

    fix : fix findVideoController.ts

--- src/videos/findVideoController.ts
+++ src/videos/findVideoController.ts
@@ -6,10 +6,9 @@ export const findVideoController = (req: Request<{id:string}>, res: Response) =>
 
     const foundedVideo = db.videos.find((video:VideoDBType)=>video.id === +req.params.id)
 
-    if(!foundedVideo){
+    if(!foundedVideo || req.params.id === undefined){
         res.sendStatus(404)
-        return
     }
 
-    res.sendStatus(200).json(foundedVideo)
+    res.status(200).json(foundedVideo)
 }
