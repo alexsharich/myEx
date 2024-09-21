d4891e270d1666140b80700c849d90d82529f8fb
alexsharich <alexsharich@gmail.com>
2024-09-22 02:53:56 +0300

    fix : fix errorsMessages

--- src/videos/createVideoController.ts
+++ src/videos/createVideoController.ts
@@ -25,7 +25,7 @@ const inputValidation = (video: any) => {
         })
     }
     if (!Array.isArray(video.availableResolutions)
-        || !video.availableResolutions.every(item => Object.values(Resolutions).includes(item))
+        || !Resolutions.includes(video.availableResolutions)
     ) {
         errors.errorsMessages.push({
             message: 'error!!!!', field: 'availableResolutions'
