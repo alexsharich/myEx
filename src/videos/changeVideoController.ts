45192e8e7f6ecf4f61f63220a685b6873370b7d3
alexsharich <alexsharich@gmail.com>
2024-09-22 04:06:25 +0300

    fix : fix errorsMessages

--- src/videos/changeVideoController.ts
+++ src/videos/changeVideoController.ts
@@ -80,7 +80,7 @@ export const changeVideoController = (req: RequestWithParamsAndBodyHW1<{
         foundedVideo.availableResolutions = req.body.availableResolutions,
         foundedVideo.canBeDownloaded = req.body.canBeDownloaded || false,
         foundedVideo.minAgeRestriction = req.body.minAgeRestriction || null,
-        foundedVideo.publicationDate = new Date(req.body.publicationDate).getTime()
+        foundedVideo.publicationDate = req.body.publicationDate
 
     db.videos = db.videos.map((video: VideoDBType) => video.id === +req.params.id ? {...foundedVideo} : video)
     res.sendStatus(204)
